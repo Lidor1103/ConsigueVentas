98ee9eeececf9f7d5768fe2249a32d726d3a7cb4
Lídor1103 <lidor.piero@gmail.com>
2023-07-05 10:44:05 -0500

    proyectos

--- js/preguntasFrecuentes.js
+++ js/preguntasFrecuentes.js
@@ -33,6 +33,37 @@ window.addEventListener("pageshow", function (event) {
 });
 
 
-function abrirVentana() {
-	window.open("https://www.youtube.com/watch?v=tM6xIFFmi7A", "_blank");
-  }
+
+// Obtenemos todos los elementos con la clase "proyecto"
+const proyectos = document.getElementsByClassName("po_hover");
+
+
+// Agregamos un evento de clic a cada elemento
+for (let i = 0; i < proyectos.length; i++) {
+  proyectos[i].addEventListener("click", function() {
+    // Redirigir a la página nueva con el PDF correspondiente
+    switch (this.id) {
+      case "proyecto1":
+        window.location.href = "portafolio/mahal.pdf";
+        break;
+      case "proyecto2":
+        window.location.href = "portafolio/jf.pdf";
+        break;
+      case "proyecto3":
+        window.location.href = "portafolio/Psicocenter.pdf";
+        break;
+		case "proyecto4":
+        window.location.href = "portafolio/sergenind.pdf";
+        break;
+		case "proyecto5":
+        window.location.href = "portafolio/ImpactoVerde.pdf";
+        break;
+		case "proyecto6":
+        window.location.href = "portafolio/Allan_E.pdf";
+        break;
+      default:
+        // Redirigir a una página de error o hacer algo más si es necesario
+        break;
+    }
+  });
+}
