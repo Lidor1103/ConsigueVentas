12268c8fef9950746c5d0c9e437d1b0a2cd91765
Lídor1103 <lidor.piero@gmail.com>
2023-07-06 14:32:57 -0500

    project

--- js/verCadaProyecto.js
+++ js/verCadaProyecto.js
@@ -14,22 +14,22 @@ const imagencambiante = document.getElementById("imagencambiante");
 // Cambia el src del elemento "embed" según el proyecto seleccionado
 switch (proyecto) {
 	case 'proyecto1':
-		imagencambiante.src = 'img/pro_mahal.png';
+		imagencambiante.src = 'img/pro_mahal.webp';
 		break;
 	case 'proyecto2':
-		imagencambiante.src = 'img/pro_jf.png';
+		imagencambiante.src = 'img/pro_jf.webp';
 		break;
 	case 'proyecto3':
-		imagencambiante.src = 'img/pro_psicocenters.png';
+		imagencambiante.src = 'img/pro_psicocenters.webp';
 		break;
 	case 'proyecto4':
-		imagencambiante.src = 'portafolio/sergenind.pdf';
+		imagencambiante.src = 'portafolio/sergenind.webp';
 		break;
 	case 'proyecto5':
-		imagencambiante.src = 'img/pro_impactoverde.png';
+		imagencambiante.src = 'img/pro_impactoverde.webp';
 		break;
 	case 'proyecto6':
-		imagencambiante.src = 'img/pro_allan.png';
+		imagencambiante.src = 'img/pro_allan.webp';
 		break;
 	default:
 		// Acciones por defecto si el proyecto no coincide con ninguno de los casos anteriores
