560ca13c81c8a90115b703077c43d5c0edbbb37a
Lídor1103 <lidor.piero@gmail.com>
2023-07-06 17:32:08 -0500

    project

--- js/verCadaProyecto.js
+++ js/verCadaProyecto.js
@@ -13,45 +13,45 @@ const imagencambiante = document.getElementById("imagencambiante");
 
 // Cambia el src del elemento "embed" según el proyecto seleccionado
 switch (proyecto) {
-  case 'proyecto1':
-    imagencambiante.src = 'img/pro_mahal.webp';
-    break;
-  case 'proyecto2':
-    imagencambiante.src = 'img/pro_jf.webp';
-    break;
-  case 'proyecto3':
-    imagencambiante.src = 'img/pro_psicocenters.webp';
-    break;
-  case 'proyecto4':
-    imagencambiante.src = 'img/pro_sergenind.webp';
-    break;
-  case 'proyecto5':
-    imagencambiante.src = 'img/pro_impactoverde.webp';
-    break;
-  case 'proyecto6':
-    imagencambiante.src = 'img/pro_allan.webp';
-    break;
-  default:
-    // Acciones por defecto si el proyecto no coincide con ninguno de los casos anteriores
-    break;
+	case 'proyecto1':
+		imagencambiante.src = 'img/pro_mahal.webp';
+		break;
+	case 'proyecto2':
+		imagencambiante.src = 'img/pro_jf.webp';
+		break;
+	case 'proyecto3':
+		imagencambiante.src = 'img/pro_psicocenters.webp';
+		break;
+	case 'proyecto4':
+		imagencambiante.src = 'img/pro_sergenind.webp';
+		break;
+	case 'proyecto5':
+		imagencambiante.src = 'img/pro_impactoverde.webp';
+		break;
+	case 'proyecto6':
+		imagencambiante.src = 'img/pro_allan.webp';
+		break;
+	default:
+		// Acciones por defecto si el proyecto no coincide con ninguno de los casos anteriores
+		break;
 }
 
 const urlPDFs = {
-  proyecto1: 'portafolio/mahal.pdf',
-  proyecto2: 'portafolio/jf.pdf',
-  proyecto3: 'portafolio/psicocenters.pdf',
-  proyecto4: 'portafolio/Sergenind.pdf',
-  proyecto5: 'portafolio/ImpactoVerde.pdf',
-  proyecto6: 'portafolio/AllanEmmanuel.pdf',
+	proyecto1: 'portafolio/mahal.pdf',
+	proyecto2: 'portafolio/jf.pdf',
+	proyecto3: 'portafolio/psicocenters.pdf',
+	proyecto4: 'portafolio/Sergenind.pdf',
+	proyecto5: 'portafolio/ImpactoVerde.pdf',
+	proyecto6: 'portafolio/AllanEmmanuel.pdf',
 };
 
 const botonDescarga = document.querySelector('#pdf_descarga');
 
-botonDescarga.addEventListener('click', function() {
-  if (proyecto in urlPDFs) {
-    const urlPDF = urlPDFs[proyecto];
-    botonDescarga.href = urlPDF;
-  } else {
-    // Acciones por defecto si el proyecto no coincide con ninguno de los casos anteriores
-  }
+botonDescarga.addEventListener('click', function () {
+	if (proyecto in urlPDFs) {
+		const urlPDF = urlPDFs[proyecto];
+		botonDescarga.href = urlPDF;
+	} else {
+		// Acciones por defecto si el proyecto no coincide con ninguno de los casos anteriores
+	}
 });
