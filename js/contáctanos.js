33b4e844064891c68c479136cb57fa421088edc8
Lídor1103 <lidor.piero@gmail.com>
2023-07-22 11:46:31 -0500

    go

--- js/contáctanos.js
+++ js/contáctanos.js
@@ -2,3 +2,25 @@
 AOS.init({
 	duration: 1000,
 });
+
+//FORMULARIO solo cuando los campos estén llenos
+function validarFormulario() {
+	// Obtener los valores de los campos
+	var nombres = document.getElementById('Nombres').value;
+	var celular = document.getElementById('Celular').value;
+	var correo = document.getElementById('Correo electrónico').value;
+	var ciudad = document.getElementById('ciudad').value;
+	var asunto = document.getElementById('Asunto').value;
+
+	// Verificar si los campos están vacíos
+	if (nombres === '' || celular === '' || correo === '' || ciudad === '' || asunto === '') {
+		// Mostrar mensaje de alerta "Faltan datos"
+		alert('Faltan datos, por favor complete todos los campos antes de enviar el formulario.');
+
+		// Evitar que el formulario se envíe
+		return false;
+	}
+	
+	// Si todos los campos están llenos, el formulario se enviará normalmente
+	return true;
+}
--- js/verCadaProyecto.js
+++ js/verCadaProyecto.js
@@ -55,3 +55,25 @@ botonDescarga.addEventListener('click', function () {
 		// Acciones por defecto si el proyecto no coincide con ninguno de los casos anteriores
 	}
 });
+
+//FORMULARIO solo cuando los campos estén llenos
+function validarFormulario() {
+	// Obtener los valores de los campos
+	var nombres = document.getElementById('Nombres').value;
+	var celular = document.getElementById('Celular').value;
+	var correo = document.getElementById('Correo electrónico').value;
+	var ciudad = document.getElementById('ciudad').value;
+	var asunto = document.getElementById('Asunto').value;
+
+	// Verificar si los campos están vacíos
+	if (nombres === '' || celular === '' || correo === '' || ciudad === '' || asunto === '') {
+		// Mostrar mensaje de alerta "Faltan datos"
+		alert('Faltan datos, por favor complete todos los campos antes de enviar el formulario.');
+
+		// Evitar que el formulario se envíe
+		return false;
+	}
+	
+	// Si todos los campos están llenos, el formulario se enviará normalmente
+	return true;
+}
