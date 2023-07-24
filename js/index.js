2770a8240cff786c32703dab8568db76cdcfe916
Lídor1103 <lidor.piero@gmail.com>
2023-07-23 22:38:51 -0500

    go

--- js/index.js
+++ js/index.js
@@ -3,3 +3,34 @@ AOS.init({
 	duration: 1000,
 });
 
+//Preguntas frecuentes
+const preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta');
+preguntas.forEach((pregunta) => {
+
+	pregunta.addEventListener('click', (e) => {
+		e.currentTarget.classList.toggle('activa');
+
+		const respuesta = pregunta.querySelector('.respuesta');
+		const alturaRealRespuesta = respuesta.scrollHeight;
+
+		if (!respuesta.style.maxHeight) {
+			respuesta.style.maxHeight = alturaRealRespuesta + 'px';
+		} else {
+			respuesta.style.maxHeight = null;
+		}
+
+		preguntas.forEach((elemento) => {
+			if (pregunta !== elemento) {
+				elemento.classList.remove('activa');
+				elemento.querySelector('.respuesta').style.maxHeight = null;
+			}
+		});
+	});
+});
+window.addEventListener("pageshow", function (event) {
+	var form = document.getElementsByTagName("form")[0];
+	if (event.persisted || (window.performance && window.performance.navigation.type === 2)) {
+		form.reset();
+	}
+});
+
