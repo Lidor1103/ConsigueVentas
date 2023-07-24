abc9ce5cea9d9c5f28adc29e739062c24947d783
Lídor1103 <lidor.piero@gmail.com>
2023-07-23 22:31:40 -0500

    go

--- js/index.js
+++ js/index.js
@@ -43,8 +43,6 @@ for (let i = 0; i < proyectos.length; i++) {
 	proyectos[i].addEventListener("click", function () {
 		// Obtener el nombre del proyecto
 		const proyecto = this.id;
-
-		// Redirigir a la página "OhMeVengo.html" pasando el nombre del proyecto como parámetro
 		window.location.href = `verproyecto.html?project=${proyecto}`;
 	});
 }
@@ -89,24 +87,3 @@ document.getElementById("botoncatalogo").addEventListener('click', function () {
 	llenarCamposFormulario(asunto, consulta);
 });
 
-//FORMULARIO solo cuando los campos estén llenos
-function validarFormulario() {
-	// Obtener los valores de los campos
-	var nombres = document.getElementById('Nombres').value;
-	var celular = document.getElementById('Celular').value;
-	var correo = document.getElementById('Correo electrónico').value;
-	var ciudad = document.getElementById('ciudad').value;
-	var asunto = document.getElementById('Asunto').value;
-
-	// Verificar si los campos están vacíos
-	if (nombres === '' || celular === '' || correo === '' || ciudad === '' || asunto === '') {
-		// Mostrar mensaje de alerta "Faltan datos"
-		alert('Faltan datos, por favor complete todos los campos antes de enviar el formulario.');
-
-		// Evitar que el formulario se envíe
-		return false;
-	}
-	
-	// Si todos los campos están llenos, el formulario se enviará normalmente
-	return true;
-}
